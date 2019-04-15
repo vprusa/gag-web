dcf4e50a42e464fea6c560c9828ee3e52a0e47b9
Miloslav Zezulka <zezulka95@gmail.com>
2019-04-15 21:16:37 +0200

    Add accessors to classes and define equality.
    Defining equality in practice here means implementing
    equals and hashCode (with respect to JPA).

--- src/main/java/cz/muni/fi/gag/app/web/domain/HandDevice.java
+++ src/main/java/cz/muni/fi/gag/app/web/domain/HandDevice.java
@@ -1,6 +1,7 @@
 package cz.muni.fi.gag.app.web.domain;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import javax.persistence.Column;
@@ -20,7 +21,7 @@ public class HandDevice {
 	protected long id;
 	
 	@NotNull
-	@Column(nullable = false)
+	@Column(nullable = false, unique = true)
 	private String deviceId;
 
 	@ManyToOne
@@ -28,5 +29,57 @@ public class HandDevice {
     private User user;
 
     @OneToMany(mappedBy = "device", orphanRemoval = true)
-    private List<SensorOffset> offset = new ArrayList<>();
+    private List<SensorOffset> offsets = new ArrayList<>();
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getDeviceId() {
+		return deviceId;
+	}
+
+	public void setDeviceId(String deviceId) {
+		this.deviceId = deviceId;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public List<SensorOffset> getOffsets() {
+		return Collections.unmodifiableList(offsets);
+	}
+
+	public void setOffsets(List<SensorOffset> offsets) {
+		this.offsets = offsets;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((deviceId == null) ? 0 : deviceId.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+            return true;
+        }
+        if (!(obj instanceof HandDevice)) {
+            return false;
+        }
+        final HandDevice other = (HandDevice) obj;
+		return getDeviceId() != null && getDeviceId().equals(other.getDeviceId());
+	}
 }
