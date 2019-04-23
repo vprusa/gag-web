320300a5a68efe196270f8e2ede08906e8991ce0
Vojtěch Průša <prusa.vojtech@gmail.com>
2019-04-23 14:45:06 +0000

    Added gesture dao

---
+++ persistence/src/main/java/cz/muni/fi/gag/web/dao/GestureDao.java
@@ -0,0 +1,14 @@
+package cz.muni.fi.gag.web.dao;
+
+import cz.muni.fi.gag.web.entity.Gesture;
+
+/**
+ * 
+ * @author Vojtech Prusa
+ * 
+ * @GestureDaoImpl
+ *
+ */
+public interface GestureDao extends GenericDao<Gesture> {
+
+}
