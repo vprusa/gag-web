018a35e30a10f9ccb4bff20333964f0ba689765b
Votjtěch Průša <prusa.vojtech@gmail.com>
2019-04-27 20:29:09 +0200

    Added User DAO & Impl

---
+++ persistence/src/main/java/cz/muni/fi/gag/web/dao/UserDao.java
@@ -0,0 +1,14 @@
+package cz.muni.fi.gag.web.dao;
+
+import cz.muni.fi.gag.web.entity.User;
+
+/**
+ * 
+ * @author Vojtech Prusa
+ * 
+ * @GestureDaoImpl
+ *
+ */
+public interface UserDao extends GenericDao<User> {
+
+}
