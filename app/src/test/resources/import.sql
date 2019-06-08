cd9cb9982e043d98454c0f1412d2c19324b9f822
Vojtěch Průša <prusa.vojtech@gmail.com>
2019-06-08 18:37:46 +0000

    Ui

--- app/src/test/resources/import.sql
+++
@@ -1,6 +0,0 @@
-
-/* 
-INSERT INTO User (role, email, firstName, lastName, passwordHash) VALUES ('ADMIN', 'adminInvalidMail', 'testAdmin', 'testAdmin', 'pass') 
-
-INSERT INTO User (role, email, firstName, lastName, passwordHash) VALUES ('USER', 'userInvalidMail', 'testUser', 'testUser', 'pass') 
-*/
