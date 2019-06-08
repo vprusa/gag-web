c400acbeca4ebb94930458fb0ee8a6e48326e4a3
Votjtěch Průša <prusa.vojtech@gmail.com>
2019-06-08 15:47:15 +0200

    Fixes and UI changes

--- app/live/gag-web-app-1.0-SNAPSHOT.WAR/partials/userController.js
+++ app/live/gag-web-app-1.0-SNAPSHOT.WAR/partials/userController.js
@@ -1,8 +1,21 @@
 'use strict';
+<<<<<<< HEAD
+=======
+/*
+angular.module('app').controller('UserController',
+		['$scope', '$location', '$route', 'commonTools', 'createUpdateTools', 
+			function ($scope, $location, $route, commonTools, createUpdateTools) {
+			$scope.title = '(Gyro-Accelerometric|Gestures Automation) Glove!';
+		} ]);
+*/
+
+
+>>>>>>> Fixes and UI changes
 
 angular.module('app')
     .controller('UserController', ['$scope', '$location', '$route', 'commonTools', 'createUpdateTools', 
     	function ($scope, $location, $route, commonTools, createUpdateTools) {
+<<<<<<< HEAD
         commonTools.getCurrentUserDetail().then(function (response) {
             $scope.user = response;
             //console.log(response);
@@ -17,4 +30,12 @@ angular.module('app')
             $scope.alerts.splice(index, 1);
         };
         
+=======
+        commonTools.getSongsForUser().then(function (response) {
+            $scope.songs = response;
+        }, function (response) {
+            $scope.alerts.push({type: 'danger', title: 'Error '+ response.status, msg: response.statusText});
+        });
+
+>>>>>>> Fixes and UI changes
     }]);
